72a366302ef7b2dae82952f7ca16b7f9bdaf200c
Aliaxei Hlushakou <alexeyka981@gmail.com>
2018-10-06 12:14:56 +0300

    good enough

--- src/index.js
+++ src/index.js
@@ -1,3 +1,45 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
+  if (array.length == 0) {
+    return 0;
 }
+
+array.sort(function(a, b){
+  return a - b;
+})
+
+let longest = 1;
+let current = 1;
+
+for (let i = 1; i < array.length; i++) {
+    if (array[i] != array[i-1]) {
+        if (array[i] == array[i-1]+1) {
+            current++;
+        }
+        else {
+            longest = Math.max(longest, current);
+            current = 1;
+        }
+    }
+}
+
+return Math.max(longest, current);
+}
+  // set = {};
+  // let longest = 0;
+  // let curr = 0;
+  // for(let num = 0; num < array.length; num++){
+  //   set[num] = true;
+  // }
+  // for(num in set){
+  //   if(set[num - 1]){
+  //     curr = num;
+  //     var count = 1;
+  //     while(set[curr + 1]){
+  //       count++;
+  //       curr++;
+  //     }
+  //     longest = Math.max(longest, count);
+  //   }
+  // }
+  // return longest;
+
